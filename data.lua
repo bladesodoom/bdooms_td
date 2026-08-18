78ae83169c6d172c2db17858d12ebe876fad93f9
Dalas Neff <214145736+bladesodoom@users.noreply.github.com>
2026-08-18 07:21:02 -0600

    Removes turrets and related files

--- data.lua
+++ data.lua
@@ -3,16 +3,6 @@
 require("prototypes.science.biter_essence")
 require("prototypes.core")
 
-require("prototypes.ammo-types.rapid-rounds")
-require("prototypes.ammo-types.mortar-shells")
-require("prototypes.ammo-types.sniper-rounds")
-require("prototypes.ammo-types.shotgun-shells")
-
-require("prototypes.turrets.rapid-turret")
-require("prototypes.turrets.mortar-turret")
-require("prototypes.turrets.sniper-turret")
-require("prototypes.turrets.shotgun-turret")
-
 require("prototypes.biters.swarm-biter")
 require("prototypes.biters.swarm-spitter")
 require("prototypes.biters.tank-biter")
@@ -21,12 +11,4 @@ require("prototypes.biters.boss-biter")
 require("prototypes.biters.nests")
 require("prototypes.biters.vanilla-biters")
 
-require("prototypes.technology.rapid-turret")
-require("prototypes.technology.mortar-turret")
-require("prototypes.technology.sniper-turret")
-require("prototypes.technology.shotgun-turret")
 require("prototypes.technology.essence-chains")
-
--- Must run last: locks turret/ammo recipes behind vanilla material techs by
--- scanning the recipes those files above just registered.
-require("prototypes.technology.material-gating")
